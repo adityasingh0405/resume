afced82378f9d1ecdc906558e0db62b0d4cb478a
adityasingh0405 <aditabhi9@gmail.com>
2025-05-20 23:12:29 +0530

    minor css changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,13 @@ import '../App.css';
 
 export default function Navbar({ setPage }) {
   return (
-    <nav className="absolute top-8 left-1/2 transform -translate-x-1/2 bg-[#111111] border-green-400 rounded-lg shadow-lg z-50 px-8 py-4 flex gap-4 items-center border-2 backdrop-blur-md">
+   <nav
+  style={{ boxShadow: `2px 6px 3px #000000,
+                       3px 6px 2px #2b5b3c,
+                       0 0 100px rgb(47, 94, 78)` }}
+  className="absolute top-8 left-1/2 transform -translate-x-1/2 bg-[#111111] border-green-400 rounded-lg shadow-lg z-50 px-8 py-4 flex gap-4 items-center border-2 backdrop-blur-md"
+>
+
       <button
         onClick={() => setPage('hero')}
         className="text-white text-xl font-medium pixelify-sans-800 hover:text-green-400 transition-all"
